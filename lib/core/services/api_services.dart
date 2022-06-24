d5bb96972add62539e1999750d166f33ec2bb0a7
Haroon khan <haroonkhan9426@gmail.com>
2022-06-24 13:22:29 +0500

    improve api services functions error logging and responses

--- lib/core/services/api_services.dart
+++ lib/core/services/api_services.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_antonx_boilerplate/core/config/config.dart';
 import 'package:flutter_antonx_boilerplate/core/models/responses/base_responses/request_response.dart';
+import 'package:flutter_antonx_boilerplate/core/others/logger_customizations/custom_logger.dart';
 import 'package:flutter_antonx_boilerplate/core/services/local_storage_service.dart';
-
-import '../../locator.dart';
+import 'package:flutter_antonx_boilerplate/locator.dart';
 
 class ApiServices {
+  final log = CustomLogger(className: 'ApiServices');
   final _config = locator<Config>();
   Future<Dio> launchDio() async {
     String? accessToken = locator<LocalStorageService>().accessToken;
@@ -14,6 +14,7 @@ class ApiServices {
     dio.interceptors.add(LogInterceptor(responseBody: true, requestBody: true));
     // dio.interceptors.add(
     //     DioCacheManager(CacheConfig(baseUrl: EndPoint.baseUrl)).interceptor);
+    dio.options.baseUrl = _config.baseUrl;
     dio.options.headers['Content-Type'] = 'application/json';
     dio.options.headers["accept"] = 'application/json';
     dio.options.headers["Authorization"] = 'Bearer $accessToken';
@@ -30,66 +31,123 @@ class ApiServices {
   }
 
   get({required String endPoint, params}) async {
-    Dio dio = await launchDio();
-    final response = await dio
-        .get('${_config.baseUrl}/$endPoint', queryParameters: params)
-        .catchError((e) {
-      debugPrint('Unexpected Error');
-    });
-    if (response.statusCode == 200) {
-      return RequestResponse.fromJson(response.data);
-    } else if (response.statusCode == 500) {
-      return RequestResponse(false, error: 'Server Error');
-    } else {
-      return RequestResponse(false, error: 'Network Error');
+    try {
+      Dio dio = await launchDio();
+      final response = await dio.get(endPoint, queryParameters: params);
+      if (response.statusCode == 200) {
+        return RequestResponse.fromJson(response.data);
+      }
+    } on DioError catch (e) {
+      ///
+      /// The request was made and the server responded with a status code
+      /// that falls out of the range of 2xx and is also not 304.
+      ///
+      if (e.response != null) {
+        log.e(
+            '@get - Server error \n Data: \n${e.response!.data} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Server error with error code: ${e.response!.statusCode}');
+      } else {
+        ///
+        /// Something happened in setting up or sending the request that triggered an Error
+        ///
+
+        log.e(
+            '@get - Internal dio error \n Request options: \n${e.requestOptions} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Internet request failed with error: ${e.message}. Please make sure you have stable internet connection');
+      }
     }
   }
 
   post({required String endPoint, data}) async {
-    Dio dio = await launchDio();
-    final response = await dio
-        .post('${_config.baseUrl}/$endPoint', data: data)
-        .catchError((e) {
-      debugPrint('Unexpected Error');
-    });
-    if (response.statusCode == 200) {
-      return RequestResponse.fromJson(response.data);
-    } else if (response.statusCode == 500) {
-      return RequestResponse(false, error: 'Server Error');
-    } else {
-      return RequestResponse(false, error: 'Network Error');
+    try {
+      Dio dio = await launchDio();
+      final response = await dio.post(endPoint, data: data);
+      if (response.statusCode == 200) {
+        return RequestResponse.fromJson(response.data);
+      }
+    } on DioError catch (e) {
+      ///
+      /// The request was made and the server responded with a status code
+      /// that falls out of the range of 2xx and is also not 304.
+      ///
+      if (e.response != null) {
+        log.e(
+            '@get - Server error \n Data: \n${e.response!.data} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Server error with error code: ${e.response!.statusCode}');
+      } else {
+        ///
+        /// Something happened in setting up or sending the request that triggered an Error
+        ///
+
+        log.e(
+            '@get - Internal dio error \n Request options: \n${e.requestOptions} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Internet request failed with error: ${e.message}. Please make sure you have stable internet connection');
+      }
     }
   }
 
   put({required String endPoint, data}) async {
-    Dio dio = await launchDio();
-    final response = await dio
-        .put('${_config.baseUrl}/$endPoint', data: data)
-        .catchError((e) {
-      debugPrint('Unexpected Error');
-    });
-    if (response.statusCode == 200) {
-      return RequestResponse.fromJson(response.data);
-    } else if (response.statusCode == 500) {
-      return RequestResponse(false, error: 'Server Error');
-    } else {
-      return RequestResponse(false, error: 'Network Error');
+    try {
+      Dio dio = await launchDio();
+      final response = await dio.put(endPoint, data: data);
+      if (response.statusCode == 200) {
+        return RequestResponse.fromJson(response.data);
+      }
+    } on DioError catch (e) {
+      ///
+      /// The request was made and the server responded with a status code
+      /// that falls out of the range of 2xx and is also not 304.
+      ///
+      if (e.response != null) {
+        log.e(
+            '@get - Server error \n Data: \n${e.response!.data} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Server error with error code: ${e.response!.statusCode}');
+      } else {
+        ///
+        /// Something happened in setting up or sending the request that triggered an Error
+        ///
+
+        log.e(
+            '@get - Internal dio error \n Request options: \n${e.requestOptions} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Internet request failed with error: ${e.message}. Please make sure you have stable internet connection');
+      }
     }
   }
 
   delete({required String endPoint, params}) async {
-    Dio dio = await launchDio();
-    final response = await dio
-        .delete('${_config.baseUrl}/$endPoint', queryParameters: params)
-        .catchError((e) {
-      debugPrint('Unexpected Error');
-    });
-    if (response.statusCode == 200) {
-      return RequestResponse.fromJson(response.data);
-    } else if (response.statusCode == 500) {
-      return RequestResponse(false, error: 'Server Error');
-    } else {
-      return RequestResponse(false, error: 'Network Error');
+    try {
+      Dio dio = await launchDio();
+      final response = await dio.delete('${_config.baseUrl}/$endPoint',
+          queryParameters: params);
+      if (response.statusCode == 200) {
+        return RequestResponse.fromJson(response.data);
+      }
+    } on DioError catch (e) {
+      ///
+      /// The request was made and the server responded with a status code
+      /// that falls out of the range of 2xx and is also not 304.
+      ///
+      if (e.response != null) {
+        log.e(
+            '@get - Server error \n Data: \n${e.response!.data} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Server error with error code: ${e.response!.statusCode}');
+      } else {
+        ///
+        /// Something happened in setting up or sending the request that triggered an Error
+        ///
+
+        log.e(
+            '@get - Internal dio error \n Request options: \n${e.requestOptions} \n Request Header: ${e.response!.headers} \n Request Options: ${e.response!.requestOptions}');
+        return RequestResponse.fromJson(
+            'Internet request failed with error: ${e.message}. Please make sure you have stable internet connection');
+      }
     }
   }
 }
